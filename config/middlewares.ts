7dee8b4e6d67690202ebb06b8cdf6d2a29147987
lucifer <lucifer@192.168.1.5>
2025-08-26 21:56:01 -0400

    add vercel other origin link

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,6 +9,7 @@ export default [
       headers: '*',
       origin: [
         'https://prodfront-one.vercel.app', 
+        'https://prodfront-git-main-shillers-projects-af08f62e.vercel.app',
         'https://pretty-novelty-2b255ff22b.strapiapp.com', 
       ],
       keepHeader: true,
